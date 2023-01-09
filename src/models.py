2b29c066dcc17dc324c0223dbe4fc1159a9b0853
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-01-09 18:09:44 +0000

    ER Modeling

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,7 +8,7 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+""" class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
@@ -27,7 +27,54 @@ class Address(Base):
     person = relationship(Person)
 
     def to_dict(self):
-        return {}
+        return {} """
+
+roles_users = Table(
+    "roles_users",
+    Base.metadata,
+    Column("roles_id", Integer, ForeignKey("roles.id"), primary_key=True),
+    Column("users_id", Integer, ForeignKey("users.id"), primary_key=True)
+)
+
+class Role(Base):
+    __tablename__ = "roles"
+    id = Column(Integer, primary_key=True)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    profile = relationship("Profile", uselist=False) # object
+    # roles = relationship("Role", secondary="roles_users") # list
+    roles = relationship("Role", secondary=roles_users, backref="users")
+
+""" 
+class RoleUser(Base):
+    __tablename__ = "roles_users"
+    roles_id = Column(Integer, ForeignKey("roles.id"), primary_key=True)
+    users_id = Column(Integer, ForeignKey("users.id"), primary_key=True) 
+"""
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True)
+
+class Priority(Base):
+    __tablename__ = "priorities"
+    id = Column(Integer, primary_key=True)
+
+class Todo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True)
+    users_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user = relationship("User", backref="todos", uselist=False)
+    priorities_id = Column(Integer, ForeignKey("priorities.id"), nullable=False)
+    priority = relationship("Priority", backref="todos", uselist=False)
+
+class Profile(Base):
+    __tablename__ = "profiles"
+    id = Column(Integer, primary_key=True)
+    users_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+        
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
